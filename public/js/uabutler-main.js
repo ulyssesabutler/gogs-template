f7cec8bfa92d2ec9fd56713a3eb77189c66ab9ee
root <root@amsterdam.uabutler.com>
2023-02-19 18:00:34 -0500

    Created template for the uabutler Gogs server

---
+++ public/js/uabutler-main.js
@@ -0,0 +1,14 @@
+$(document).ready(function()
+{
+    // fix main menu to page on passing
+    $('#main-menu').visibility(
+    {
+        type: 'fixed'
+    });
+
+    // show dropdown on hover
+    $('#main-menu  .ui.dropdown').dropdown(
+    {
+        on: 'hover'
+    });
+});
